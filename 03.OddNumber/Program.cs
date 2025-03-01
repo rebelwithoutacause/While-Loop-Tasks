6be6339c1ba4f211109620dfa594239db2a69ae3
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-01 11:05:10 +0200

    Add files via upload

---
+++ 03.OddNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿//Write a program to enter an odd number:
+//•	Read numbers from the console until an odd number is entered
+//•	Print the odd number as output
+
+int number = int.Parse(Console.ReadLine());
+
+while (number % 2 == 0) 
+{
+ number = int. Parse(Console.ReadLine());
+}
+Console.WriteLine(number); number++;
---
+++ 04.Sequence2k+1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Write a program to print a sequence of numbers:
+//•	The first number is 1
+//•	Each next number is 2 times the previous number + 1
+//•	Read an integer number n from the console – the max number
+//•	Print the elements of the sequence (starting with 1), which are ≤ n
+
+int maxNumber = int.Parse(Console.ReadLine());
+int n = 1;
+
+while (n <= maxNumber) 
+{
+    Console.WriteLine(n); 
+        n = n * 2 + 1;
+}
